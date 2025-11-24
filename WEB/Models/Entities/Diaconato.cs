c9ffd174f0681df1b08e44136fc7c361c38cabe6
carlosviso <carlosviso@sepog.ro.gov.br>
2025-11-24 10:55:31 -0400

    .

--- WEB/Models/Entities/Diaconato.cs
+++ WEB/Models/Entities/Diaconato.cs
@@ -2,13 +2,15 @@
 
 public class Diaconato
 {
-    public Guid DiaconatoId  { get; set; }
+    public Guid DiaconatoId { get; set; }
     public string? NomeCompleto { get; set; }
     public string? Regiao { get; set; }
     public string? Igreja { get; set; }
     public string? Cargo { get; set; }
     public string? Contato { get; set; }
     public string? NomePastor { get; set; }
+    public string? NomeSuperintendenteRegional { get; set; }
+    public string? NomeSuperintendenteEstadual { get; set; }
     public DateTime? DataNascimento { get; set; }
     public DateTime? DataMinisterio { get; set; }
     public DateTime? DataBatismo { get; set; }
